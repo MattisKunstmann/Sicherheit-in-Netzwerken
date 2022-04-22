31814a66d35f2940b4bebf2f5723b3b6c18adc76
2LuckyLuke <luwee99@gmail.com>
2022-04-22 15:16:15 +0200

    changes for task 3

--- flaskServer.py
+++ flaskServer.py
@@ -13,7 +13,7 @@ serverPort = 8080
 db = null;
 
 query1 = "select * from zugangsdaten"
-query2 = "insert into zugangsdaten values (%s,%s,%s)"
+#query2 = "insert into zugangsdaten values (%s,%s,%s)" #waay to secure 
 writeIntQuery = "insert into integers (integerNumber) values (%s)"
 dbName = "sicherheit"
 
@@ -23,23 +23,29 @@ def gfg():
     if request.method == "POST":
         name = request.form.get("name")
         password = request.form.get("password")
-        file = open('server.log', 'a')
-        file.write("Name: " + name + "Password: " + password)
-        file.close()
-        writeInDatabase((666,name,password))
-        return "OK"
+        saveDataToTextFile(name)
+        saveDataToTextFile(password)
+        #writeInDatabase((666,name,password)) #the way to secure way
+        return make_response(writeInDatabase('insert into zugangsdaten values (666, "' + name + '", "' + password + '")'))
 
 @app.route('/integer/', methods=["POST"])
 @cross_origin()
 def intToDB():
     if request.method == "POST":
         integer = request.form.get("integer")
+        saveDataToTextFile(integer)
         return make_response(writeIntToDatabase((integer,)))
 
 @app.route('/', methods=["GET"])
 def get():
     if request.method == "GET":
         return render_template("index.html")
+
+
+def saveDataToTextFile(str):
+    file = open('data.txt', 'a')
+    file.write('\n' + str)
+    file.close
 
 def initiateDatabaseConnection():
     return mysql.connector.connect(
@@ -57,9 +63,22 @@ def printTableFromDatabase():
 
 #writes an i, name and password in a table specified in "query2" in the database
 def writeInDatabase(data):
-    cursor = db.cursor()
-    cursor.execute(query2, data)
-    db.commit()
+    try:
+        cursor = db.cursor(buffered=True)
+        #cursor.execute(query2, data) #waaay to secure
+#        cursor.execute(data, multi=True)
+        cursor.execute("insert into zugangsdaten values (555, 'multitest', 'bliblablub'); select * from zugangsdaten;", multi=True)
+        result = "true";
+        try:
+            result = cursor.fetchall()
+        finally:
+            db.commit()
+            data = {'success' : str(result)}
+            return (data, 200)
+    except Exception as e:
+        print(e)
+        data = {'error' : str(e)}
+        return(data, 422)
 
 #writes an integer form the form to the table integers in the database
 def writeIntToDatabase(data):
